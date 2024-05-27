148928b4e24d768922eb34fba2292d85e288b649
Nitin-web-dev <nitinwebdev5@gmail.com>
2024-05-27 13:38:30 +0530

    inital and webpack and folder sturcture

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean : true,
+
+    },
+    devtool: 'source-map',
+    devServer : {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port : 3000,
+        hot: true,
+        historyApiFallback: true,
+        compress: true,
+        // open: true,
+    },
+    module : {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader","sass-loader","css-loader"],
+            },
+            {
+                test : /\.js$/,
+                exclude: /node_modules/, // for not include node modules files
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+
+                    }
+                },
+
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'crudappwithindexDB',
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'src/template.html')
+        })
+    ]
+
+}
